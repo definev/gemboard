5e61a79cf9f0856527ee08597facb231ee33b6a1
Definev <daiduong.workmail@gmail.com>
2024-07-12 14:13:31 +0700

    chore: change name to whiteboard

--- feature/home/lib/src/view/genit_lead_sidebar_view.dart
+++ feature/home/lib/src/view/genit_lead_sidebar_view.dart
@@ -252,7 +252,7 @@ class GenitFolderBuilder extends HookWidget {
                   child: EmojiLabel(
                     kind: genitEmojiLabelKind,
                     emoji: StyledText('➕'),
-                    label: StyledText('Create jam'),
+                    label: StyledText('Create whiteboard'),
                   ),
                 ),
               );
