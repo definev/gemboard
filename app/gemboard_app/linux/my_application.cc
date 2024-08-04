5310947acf08a1539cd26b7459d3006c3d9e06ab
definev <daiduong.workmail@gmail.com>
2024-08-04 22:02:09 +0700

    feat: update app name

--- app/gemboard_app/linux/my_application.cc
+++ app/gemboard_app/linux/my_application.cc
@@ -41,11 +41,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "gemboard_app");
+    gtk_header_bar_set_title(header_bar, "Gemboard");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "gemboard_app");
+    gtk_window_set_title(window, "Gemboard");
   }
 
   auto bdw = bitsdojo_window_from(window);            // <--- add this line
